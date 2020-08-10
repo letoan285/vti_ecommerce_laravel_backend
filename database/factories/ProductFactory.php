5537d73b8f73b1fefd7741acb2a97e16c00129fd
Le Toan <letoan@gmail.com>
2020-08-11 06:21:30 +0700

    login page

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,7 +6,14 @@ use App\Models\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
+    $name = $faker->name;
     return [
-        //
+        'name' => $name,
+        'slug' => Str::slug($name),
+        'price' => rand(1000, 5000),
+        'description' => $faker->name, // password
+        'image' => Str::random(10),
+        'category_id' => rand(1,3),
+        'supplier_id' => rand(1,3),
     ];
 });
